9979a2eb55abbbc4ad840e0a1cf6fdc196400a9d
Luke Mortimer <lbmortimer1998@gmail.com>
2019-05-21 06:51:50 +0100

    added presets

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # strangerLights
 An Arduino-Python interface for controlling a set of 26 LEDs above different letters on a wall.
 
+Features:
+ - can do words or patterns
+ - can set presets to quickly swap between patterns
+ - can change how long the LEDs spend on/off
+
 ### Dependencies
 The Python control script requires pyserial.
 
 ### Setting up
-This requires quite a lot of wire, about £20 worth. Each LED needs to be connected (with a resistor) to one of the digital i/o pins from 22 to 48 on the Arduino, where 22 is "a", 23 is "b" and so on. A reccomended setup is to have a Raspberry Pi connected which has SSH capabilities, that way you can use your phone to change the lights.
+This requires quite a lot of wire, about £20 worth. Each LED needs to be connected (with a resistor) to one of the digital i/o pins from 22 to 48 on the Arduino, where 22 is "a", 23 is "b" and so on. A recommended setup is to have a Raspberry Pi connected which has SSH capabilities, that way you can use your phone to change the lights.
 
 ### Installing
- - flash the Arduino firmware to an Arduino Mega (Mega because it has more  digital i/o pins)
+ - flash the Arduino firmware to an Arduino Mega (Mega because it has more digital i/o pins and flash memory for storing patterns)
  - connect a device with Python installed to the serial port of the Arduino
  - run the Python script
 
@@ -32,8 +37,26 @@ a,c,e,g,i,k,m,o,q,s,u,w,yb,d,f,h,j,l,n,p,r,t,v,x,z
 
 Be sure to add a space at the end if you want there to be a break before looping. Sometimes this won't be needed, especially if doing a pattern rather than a word.
 
-You can also change the oN and ofF timers in milliseconds by using:
+You can change the oN and ofF timers in milliseconds by using:
 ```
 #n1000
 #f100
 ```
+
+Presets can set using the "#p" command, such that the following will set preset 1 to be the words "preset test":
+```
+#p1preset test
+```
+
+This preset can then be accessed using a ".1", such that the following becomes "this is a preset test":
+```
+this is a .1
+```
+
+By default there are a maximum of 10 presets, where the last 5 have preset patterns:
+
+6) half on and half off, then vice versa
+7) the light bounces from the top left corner, like the DVD logo
+8) a vertical line moving from the right to the left and looping
+9) tetris
+10) a sine wave travelling to the right
--- src/sketch.ino
+++ src/sketch.ino
@@ -2,6 +2,8 @@ char charBuffer = ' ';
 String stringBuffer = "";
 
 String currentMessage = "";
+const int maxPresets = 10;
+String savedMessages[maxPresets];
 int nextLetter = 0;
 
 int startPin = 22;
@@ -13,76 +15,88 @@ int offTime = 100;
 
 void setup(){
 
-  Serial.begin(9600);
+	Serial.begin(9600);
 
-  for (int i=startPin; i <= startPin+26; i++){
+	for (int i=startPin; i <= startPin+26; i++){
 
-    pinMode(i, OUTPUT);
-    
-  }
+		pinMode(i, OUTPUT);
+		
+	}
+
+	savedMessages[0] = "";
+	savedMessages[1] = "";
+	savedMessages[2] = "";
+	savedMessages[3] = "";
+	savedMessages[4] = "";
+	
+	savedMessages[5] = "a,c,e,g,i,k,m,o,q,s,u,w,yb,d,f,h,j,l,n,p,r,t,v,x,z"; //half on
+	savedMessages[6] = "akumeoyqgowmcksi"; // bouncing
+	savedMessages[7] = "i,ra,j,sb,k,tc,l,ud,m,ve,n,wf,o,xg,p,yh,q,z"; //moving line
+	savedMessages[8] = "a,b,c,lb,c,d,mc,d,e,nd,e,f,oe,f,g,pf,g,h,qs,t,k,u,f,g,h,qt,u,v,l,f,g,h,qu,v,w,m,f,g,h,qv,w,x,n,f,g,h,qw,x,y,o,f,g,h,qx,y,z,p,f,g,h,qh,za,j,s,k,h,zb,k,t,l,h,zc,l,u,m,h,zd,m,v,n,h,ze,n,w,o,h,zf,o,x,p,h,zg,p,y,h,q,z "; //tetris
+	savedMessages[9] = "i,a,k,u,m,e,o,y,q r,j,b,l,v,n,f,p,z i,s,k,c,m,w,o,g,q j,t,l,d,n,x,p,h"; // sine wave
 
 }
 
 void loop(){
 
-  if (Serial.available() > 0) {
-    
-    charBuffer = Serial.read();
-    //Serial.print("I received char code: ");
-    //Serial.println(int(charBuffer));
+	if (Serial.available() > 0) {
+		
+		charBuffer = Serial.read();
+		//Serial.print("I received char code: ");
+		//Serial.println(int(charBuffer));
 
-    if (charBuffer == 10) {
+		if (charBuffer == 10) {
 
-      processMessage(stringBuffer);
-      stringBuffer = "";
-    
-    } else {
+			processMessage(stringBuffer);
+			stringBuffer = "";
+		
+		} else {
 
-      stringBuffer = stringBuffer + charBuffer;
-      
-    }
-    
-  } else {
+			stringBuffer = stringBuffer + charBuffer;
+			
+		}
+		
+	} else {
 
-      pin = int(currentMessage[nextLetter])-97+startPin;
+			pin = int(currentMessage[nextLetter])-97+startPin;
 
-			if (pin >= startPin && pin <= startPin+26){
+		if (pin >= startPin && pin <= startPin+26){
 
-				numPins = 1;
-				pinArray[numPins-1] = pin;
+			numPins = 1;
+			pinArray[numPins-1] = pin;
+
+			nextLetter++;
+
+			while (currentMessage[nextLetter] == ','){
 
 				nextLetter++;
+				pin = int(currentMessage[nextLetter])-97+startPin;
 
-				while (currentMessage[nextLetter] == ','){
+				if (pin >= startPin && pin <= startPin+26){
 
-					nextLetter++;
-					pin = int(currentMessage[nextLetter])-97+startPin;
-
-					if (pin >= startPin && pin <= startPin+26){
-	
-						numPins++;
-						pinArray[numPins-1] = pin;
-								
-					}
-
-					nextLetter++;
-					if (nextLetter >= currentMessage.length()){nextLetter = 0;}
-
+					numPins++;
+					pinArray[numPins-1] = pin;
+							
 				}
 
-			} else {
-
-				numPins = 0;
 				nextLetter++;
+				if (nextLetter >= currentMessage.length()){nextLetter = 0;}
 
 			}
 
-			if (nextLetter >= currentMessage.length()){nextLetter = 0;}
+		} else {
 
-			for (int i = 0; i < numPins; i++){digitalWrite(pinArray[i], HIGH);}
-			delay(onTime);
-			for (int i = 0; i < numPins; i++){digitalWrite(pinArray[i], LOW);}
-			delay(offTime);
+			numPins = 0;
+			nextLetter++;
+
+		}
+
+		if (nextLetter >= currentMessage.length()){nextLetter = 0;}
+
+		for (int i = 0; i < numPins; i++){digitalWrite(pinArray[i], HIGH);}
+		delay(onTime);
+		for (int i = 0; i < numPins; i++){digitalWrite(pinArray[i], LOW);}
+		delay(offTime);
 
 	}
 
@@ -90,9 +104,25 @@ void loop(){
 
 void processMessage(String message){
 
-  if (message[0] == '#'){
+	int j = 0;
+	int index = 0;	
+	while (j < message.length()){
 
-    Serial.println("Recieved command: " + message);
+		if (message[j] == '.'){
+
+			index = message.substring(j+1, 1).toInt();
+			message = message.substring(0, j) + savedMessages[index-1] + message.substring(j+2, message.length());
+
+		}
+
+		j++;
+
+	}
+
+
+	if (message[0] == '#'){
+
+		Serial.println("Recieved command: " + message);
 
 		if (message[1] == 'n'){
 
@@ -106,16 +136,26 @@ void processMessage(String message){
 			if (offTime+onTime < 100){offTime = 100;}
 			Serial.println("Off delay is now " + String(offTime) + " ms");
 
-		}   
+		} else if (message[1] == 'p'){
+		
+			int index = message.substring(2,1).toInt();	
+			savedMessages[index-1] = message.substring(3, message.length());
+			Serial.println("Preset " + String(index) + " is now: " + savedMessages[index-1]);
+
+		} else if (message[1] == 'r'){
+
+			onTime = 1000;
+			offTime = 100;
+
+		} 
  
-  } else {
+	} else {
 
 		if (message[0] == ','){message=" ";}
-    Serial.print("Changing message to: " + message);
-	  currentMessage = message;
-		if (currentMessage[message.length()] != ' '){currentMessage += " ";}		
-		nextLetter = 0;    
-    
-  }
+		Serial.print("Changing message to: " + message);
+		currentMessage = message;
+		nextLetter = 0;		
+		
+	}
 
 }
